2149f1c15c0817bb111450573ed579789dfa8843
surasit.fhaisatja <topsecret.top@hotmail.com>
2020-08-19 10:45:59 +0700

    Add ImageController

---
+++ OAuth2.API/Controllers/ImageController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OAuth2.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ImageController : Controller
+    {
+        [HttpGet("GetImageAll")]
+        public string GetImageAll()
+        {
+            return "Get images is success";
+        }
+    }
+}
