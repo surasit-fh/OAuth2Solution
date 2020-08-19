57026bab2dc0597b1deb69697f4bb8ca51c3d984
surasit.fhaisatja <topsecret.top@hotmail.com>
2020-08-19 10:46:12 +0700

    Add AccountController

---
+++ OAuth2.IDP/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OAuth2.IDP.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class AccountController : Controller
+    {
+        [HttpPost("Login")]
+        public string Login()
+        {
+            return "Login is success";
+        }
+
+        [HttpPost("Logout")]
+        public string Logout()
+        {
+            return "Logout is success";
+        }
+    }
+}
